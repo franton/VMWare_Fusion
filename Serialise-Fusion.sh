15ce9955c3ba75776611bd71f5a2b42f34be3462
Richard Purves <franton@users.noreply.github.com>
2015-11-03 15:25:02 -0800

    Create Serialise-Fusion.sh

---
+++ Serialise-Fusion.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script to grab and serialise vmware fusion
+
+# Author      : contact@richard-purves.com
+
+# Set up needed variables here
+
+udid=$( ioreg -rd1 -c IOPlatformExpertDevice | awk '/IOPlatformUUID/ { split($0, line, "\""); printf("%s\n", line[4]); }' )
+apiurl=`/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url`
+apiuser="apiuser"
+apipass=""
+
+# Has VMWare Fusion actually been installed?
+
+if [ ! -x /Applications/VMware\ Fusion.app/Contents/Library/Initialize\ VMware\ Fusion.tool ];
+then
+    echo "Error: VMware Fusion tool is not executable."
+	exit 1
+fi
+
+# Grab user info from extension attribute for target computer and process.
+
+# Retrieve the computer record data from the JSS API
+
+cmd="curl --silent --user ${apiuser}:${apipass} --request GET ${apiurl}JSSResource/computers/udid/$udid"
+hostinfo=$( ${cmd} )
+
+# Now parse the data and get the usernames
+
+serial=${hostinfo##*VMWare Serial\<\/name\><type>String</type><value>}
+serial=${adminusers%%\<\/value\>*}
+
+# Everything hopefully checks out. Run the serialiser.
+
+/Applications/VMware\ Fusion.app/Contents/Library/Initialize\ VMware\ Fusion.tool set "" "" ${serial}
+
+# Finished!
+
+exit 0
